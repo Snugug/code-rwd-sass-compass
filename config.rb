8ad31de46a452bfb7fe457a1e5ad0453c68263bc
Sam Richard <snugug@gmail.com>
2013-11-01 15:08:08 -0400

    Added singularity-extras to config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 require 'toolkit'
 require 'compass-normalize'
+require 'singularity-extras'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
