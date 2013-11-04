6b62fb1748d66e25ddbc6979015b1777f89b74a2
Sam Richard <snugug@gmail.com>
2013-11-04 16:15:13 -0500

    Updated Gruntfile
    Required underscore, parsed an extension var, added Bundler task, calls Bundler task on server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,16 @@
 (function() {
   'use strict';
 
+  var _ = require('underscore');
+
   module.exports = function (grunt) {
     // Read in User Config
     var userConfig = grunt.file.readYAML('config.yml');
+
+    var extensions = [];
+    _.forEach(userConfig.compass.dependencies, function(v, e) {
+      extensions.push(e);
+    });
 
     // Grunt task configuration
     grunt.initConfig({
@@ -61,6 +68,7 @@
           '!' + userConfig.dirs.scripts + '/**/*.min.js'
         ]
       },
+      // Compass
       compass: {
         options: {
           sassDir: userConfig.dirs.sass,
@@ -69,7 +77,7 @@
           javascriptsDir: userConfig.dirs.scripts,
           fontsDir: userConfig.dirs.fonts,
           relativeAssets: userConfig.compass.relative_assets,
-          require: userConfig.compass.extensions,
+          require: extensions,
           importPath: userConfig.compass.import_paths,
           bundleExec: true
         },
@@ -77,6 +85,14 @@
           options: {
             environment: 'development'
           }
+        }
+      },
+      //Exec
+      exec: {
+        bundler: {
+          command: 'bundle install',
+          stdout: true,
+          stderr: true
         }
       }
 
@@ -91,7 +107,7 @@
     //////////////////////////////
     grunt.registerTask('server', function () {
       var launch = grunt.option('launch');
-      grunt.task.run(['connect:server']);
+      grunt.task.run(['bundler', 'connect:server']);
 
       if (launch === true) {
         grunt.task.run(['open:launch']);
@@ -108,6 +124,21 @@
     //////////////////////////////
     grunt.registerTask('default', function () {
       console.log('This runs when you run `grunt`');
+    });
+
+    //////////////////////////////
+    // Bundler Task
+    //////////////////////////////
+    grunt.registerTask('bundler', function () {
+      var gemfileContent = '# Pull gems from RubyGems\nsource "https://rubygems.org"\n';
+
+      _.forEach(userConfig.compass.dependencies, function(v, e) {
+          gemfileContent += 'gem "' + e + '", "' + v + '"\n';
+      });
+
+      grunt.file.write(userConfig.dirs.root + '/Gemfile', gemfileContent);
+
+      grunt.task.run(['exec:bundler']);
     });
   };
 }());
